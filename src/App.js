a154b3d0ff2c39aa25bd6e81b677ef496ef831c0
Pavlina <pavlinakubackova1@gmail.com>
2023-07-26 13:29:34 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ListComponent from "./components/listComponent";
+//import Welcome from "./components/welcome";
+import SearchComponent from "./components/searchComponent";
+import { useState } from "react";
 
 function App() {
+  // const person = { name: "Pepa", lastName: "Novak" };
+  // const secondPerson = { name: "tom", lastName: "dvorak" };
+
+  //console.log(props);
+
+  // const handleClick = () => {
+  //   console.log("button clicked");
+  // };
+  const [searchField, setSearchField] = useState("");
+
+  const handleChange = (event) => {
+    setSearchField(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello Students</h1>
+      <SearchComponent handleChange={handleChange} />
+      <ListComponent searchString={searchField} />
     </div>
   );
 }
